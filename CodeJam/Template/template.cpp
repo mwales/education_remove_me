5b04ee9b2eb56c4c73ccbfd5747eaeef526d89a5
Michael Wales <mwales3@gmail.com>
2016-04-15 08:13:30 -0400

    Blank code jam boiler plate solution

---
+++ CodeJam/Template/template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+std::string solveCase()
+{
+
+
+
+   return "solution";
+
+}
+
+int main(int, char**)
+{
+   int numCases = 0;
+   std::cin >> numCases;
+
+   for(int i = 0; i < numCases; i++)
+   {
+      std::cout << "Case #" << i+1 << ": " << solveCase() << std::endl;
+   }
+
+   return 0;
+}
