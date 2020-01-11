208f216dff3890a6a97ad29cf31ab8752f88ce30
Michael Wales <mwales3@gmail.com>
2020-01-11 15:43:00 -0500

    Finished day 17 part 2

--- AdventOfCode/2019/day17/main.cpp
+++ AdventOfCode/2019/day17/main.cpp
@@ -9,6 +9,7 @@
 #include "Scaffold.h"
 
 
+
 std::string readFile(std::string filename)
 {
    std::ifstream f;
@@ -31,7 +32,37 @@ std::string readFile(std::string filename)
    return retVal;
 }
 
+std::vector<int64_t> stringToData(std::string input)
+{
+   std::vector<int64_t> retVal;
 
+   for(auto it = input.begin(); it != input.end(); it++)
+   {
+      retVal.push_back((int) *it);
+   }
+
+   return retVal;
+}
+
+void clearTheScreen()
+{
+   // Found this on stack overflow
+   std::cout << "\033[2J\033[1;1H";
+}
+
+void outputScreen(std::vector<int64_t> const & q)
+{
+   clearTheScreen();
+
+   for(auto it = q.begin(); it != q.end(); it++)
+   {
+      std::cout << (char) *it;
+   }
+
+   std::cout << std::endl;
+
+   usleep(100000);
+}
 
 int main(int argc, char** argv)
 {
@@ -42,7 +73,9 @@ int main(int argc, char** argv)
    std::vector<int64_t> inputQ;
    std::vector<int64_t> outputQ;
 
-   ElfComputer emu(strListToInt(split(programData, ",")), "emu");
+   std::vector<int64_t> pd = strListToInt(split(programData, ","));
+
+   ElfComputer emu(pd, "emu");
    emu.setInputDataQ(&inputQ);
    emu.setOutputDataQ(&outputQ);
 
@@ -66,6 +99,80 @@ int main(int argc, char** argv)
    int sumAlign = s.getSumAlignmentParameters();
 
    std::cout << "Sum of align params: " << sumAlign << std::endl;
+
+   sleep(5);
+
+   std::string pt2Program, funcA, funcB, funcC, liveViewOption;
+   std::cin >> pt2Program;
+   std::cin >> funcA;
+   std::cin >> funcB;
+   std::cin >> funcC;
+   std::cin >> liveViewOption;
+
+   pd[0] = 2; // Manual control mode
+   ElfComputer emuControl(pd, "control");
+   inputQ.clear();
+   outputQ.clear();
+
+   std::string theWholeThing;
+   theWholeThing = pt2Program;
+   theWholeThing += "\n";
+   theWholeThing += funcA;
+   theWholeThing += "\n";
+   theWholeThing += funcB;
+   theWholeThing += "\n";
+   theWholeThing += funcC;
+   theWholeThing += "\n";
+   theWholeThing += liveViewOption;
+   theWholeThing += "\n";
+
+   printIntList(stringToData(theWholeThing));
+
+   inputQ = stringToData(theWholeThing);
+   emuControl.setInputDataQ(&inputQ);
+   emuControl.setOutputDataQ(&outputQ);
+
+   numIterations = 0;
+
+   // When we see 2 consecutive new lines, clear the screen, output Q, delay
+   int prevQSize = 0;
+   int consecutiveNewLines = 0;
+
+   while(!emuControl.isHalted())
+   {
+      emuControl.runIteration();
+      numIterations++;
+
+      // Are the last two items in the output Q \n\n?
+      if (outputQ.size() != prevQSize)
+      {
+         if (outputQ.back() == '\n')
+         {
+            consecutiveNewLines++;
+         }
+         else
+         {
+            consecutiveNewLines = 0;
+         }
+
+         if (consecutiveNewLines == 2)
+         {
+            // Output the screen and clear the Q
+            outputScreen(outputQ);
+            outputQ.clear();
+            consecutiveNewLines = 0;
+            prevQSize = 0;
+         }
+         else
+         {
+            prevQSize = outputQ.size();
+         }
+      }
+   }
+
+   std::cout << "Dust collected: ";
+   printIntList(outputQ);
+
 
    return 0;
 }
