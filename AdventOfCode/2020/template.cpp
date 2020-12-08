426f679b78c055d84e5bdebfc85f88ea213c8242
Michael Wales <mwales3@gmail.com>
2020-12-07 23:59:33 -0500

    Added some stuff to AoC template file

--- AdventOfCode/2020/template.cpp
+++ AdventOfCode/2020/template.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <cstdlib>
+#include <set>
 
 #define AOC_DEBUG 1
 #ifdef AOC_DEBUG
@@ -8,6 +11,50 @@
 	#define DEBUG if(0) std::cout
 #endif
 
+template<typename printableType>
+void printVector(std::vector<printableType> v)
+{
+        std::string retVal;
+        bool first = true;
+        for(auto const & curItem: v)
+        {
+                if (!first)
+                {
+                        DEBUG << ",";
+                }
+                else
+                {
+                        first = false;
+                }
+
+                DEBUG << curItem;
+        }
+}
+
+template<typename sortableType>
+void insertOrdered(std::vector<sortableType>& origList, sortableType x)
+{
+        for(auto it = origList.begin(); it != origList.end(); it++)
+        {
+                if (*it > x)
+                {
+                        origList.insert(it, x);
+                        return;
+                }
+        }
+
+        // If we get here, empty list?
+        origList.push_back(x);
+}
+
+template<typename t>
+std::vector<t> append(std::vector<t> a, std::vector<t> b)
+{
+	std::vector<t> retVal;
+	retVal = a;
+	retVal.insert(retVal.end(), b.begin(), b.end());
+	return retVal;
+}
 
 
 std::vector<std::string> stringSplit(std::string const & input, char delimeter)
@@ -33,15 +80,36 @@ std::vector<std::string> stringSplit(std::string const & input, char delimeter)
 	return retVal;
 }
 
+std::string replaceChar(std::string orig, char before, char after)
+{
+	std::string retVal;
+	for(auto singleChar: orig)
+	{
+		if (singleChar == before)
+		{
+			retVal += after;
+		}
+		else
+		{
+			retVal += singleChar;
+		}
+	}
+
+	return retVal;
+}
+
 int main(int argc, char** argv)
 {
-	
+
+
 	while(1)
 	{
-
+		std::string text;
+		std::getline(std::cin,text);
 
 
 		// out of output
+check_for_eof:
 		if (std::cin.eof())
 		{
 			break;
@@ -50,6 +118,7 @@ int main(int argc, char** argv)
 
 	}
 
+	
 
 	return 0;
 }
