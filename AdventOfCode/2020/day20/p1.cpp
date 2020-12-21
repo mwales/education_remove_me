e7db0e281a125a18203d4c82dd5572f0121c40ed
Michael Wales <mwales3@gmail.com>
2020-12-21 02:43:25 -0500

    2020 AoC Day 20 complete.  6,940th place

--- AdventOfCode/2020/day20/p1.cpp
+++ AdventOfCode/2020/day20/p1.cpp
@@ -15,7 +15,6 @@
 	#define DEBUG if(0) std::cout
 #endif
 
-#define TILE_SIZE 10
 
 class Tile
 {
@@ -33,7 +32,13 @@ public:
 
 	bool matchRightSide(Tile const & rhs);
 
+	bool matchBottomSide(Tile const & rhs);
+
 	bool getState(int x, int y) const;
+
+	int searchForSeaMonster();
+
+	int getNumActiveCells();
 
 	std::vector<std::string> theData;
 
@@ -42,10 +47,493 @@ public:
 	int theNumRotations;
 
 	std::string theMatchLog;
+
+	int theTileSize;
+
+	std::set<uint64_t> theMatchingTiles;
 };
 
-bool Tile::getState(int x, int y) const
+class TileMap
 {
+public:
+	TileMap(std::vector<Tile*> tileSet);
+
+	void findCorners();
+
+	void placeCornerTopLeft();
+
+	void placeRemainingTiles();
+
+	void printEntireMap();
+
+	std::vector<std::string> createMegaTile();
+
+	Tile* getTileByNumber(uint64_t num);
+
+	std::vector<int> getCornerList();
+
+	std::vector<int> theCornerList;
+
+	std::vector<Tile*> theTiles;
+
+	std::map<std::pair<int, int>, Tile*> theMap;
+
+	int theWidth;
+
+	int theHeight;
+};
+
+
+Tile* TileMap::getTileByNumber(uint64_t num)
+{
+	for(auto t: theTiles)
+	{
+		if (t->getTileNumber() == num)
+		{
+			return t;
+		}
+	}
+	
+	DEBUG << "There is not tile number " << num << std::endl;
+	assert(false);
+	return nullptr;
+}
+
+std::vector<std::string> TileMap::createMegaTile()
+{
+	assert(theMap.size() == theTiles.size());
+	std::vector<std::string> retVal;
+
+	for(int curY = 0; curY < theHeight; curY++)
+	{
+		for(int tileRow = 1; tileRow < theTiles[0]->theTileSize - 1; tileRow++)
+		{
+
+			std::string singleRow;
+			for(int curX = 0; curX < theWidth; curX++)
+			{
+
+				for(int subX = 1; subX < theTiles[0]->theTileSize - 1; subX++)
+				{
+					singleRow += theMap[{curX, curY}]->theData[tileRow][subX];
+				}
+
+				//DEBUG << theMap[{curX, curY}]->theData[tileRow] << " ";
+
+			}
+			retVal.push_back(singleRow);
+			//DEBUG << std::endl;
+
+		}
+
+		//DEBUG << std::endl;
+	}
+
+	//DEBUG << std::endl;
+	return retVal;
+}
+
+void TileMap::printEntireMap()
+{
+	assert(theMap.size() == theTiles.size());
+
+	for(int curY = 0; curY < theHeight; curY++)
+	{
+		for(int tileRow = 0; tileRow < theTiles[0]->theTileSize; tileRow++)
+		{
+			for(int curX = 0; curX < theWidth; curX++)
+			{
+				DEBUG << theMap[{curX, curY}]->theData[tileRow] << " ";
+			}
+
+			DEBUG << std::endl;
+
+		}
+
+		DEBUG << std::endl;
+	}
+
+	DEBUG << std::endl;
+}
+
+void TileMap::placeRemainingTiles()
+{
+	int curX = 2;
+	int curY = 0;
+
+	std::set<Tile*> remainingTiles;
+	for(auto t: theTiles)
+	{
+		remainingTiles.emplace(t);
+	}
+
+	// Remove the 3 tiles we have already placed
+	remainingTiles.erase(theMap[{0,0}]);
+	remainingTiles.erase(theMap[{0,1}]);
+	remainingTiles.erase(theMap[{1,0}]);
+
+	DEBUG << "Remaining tile to place = " << remainingTiles.size() << std::endl;
+
+	// Do top row
+	while(curY == 0)
+	{
+		Tile* lt = theMap[{curX-1,curY}];
+
+		bool foundTile = false;
+		for(auto possibleTile: remainingTiles)
+		{
+
+			for(int rot = 0; rot < 8; rot++)
+			{
+				possibleTile->rotateTile();
+
+				if (lt->matchRightSide(*possibleTile))
+				{
+					// We found the next tile
+					DEBUG << "Tile at " << curX << "," << curY << " is " << possibleTile->getTileNumber() << std::endl;
+					theMap[{curX,curY}] = possibleTile;
+
+					// Don't rotate this tile anymore!
+					foundTile = true;
+					rot = 8;
+					remainingTiles.erase(possibleTile);
+				}
+			}
+
+			if (foundTile)
+				break;
+		}
+
+		if (foundTile)
+		{
+			curX++;
+			DEBUG << "Moving to next X " << curX << std::endl;
+		}
+		else
+		{
+			DEBUG << "We didn't find any more tiles on this row.  Width = " << curX << std::endl;
+			theWidth = curX;
+			theHeight = theTiles.size() / theWidth;
+			DEBUG << "The height will be " << theHeight << std::endl;
+			curY++;
+			curX = 0;
+		}
+
+	}
+
+	for(curY = 1; curY < theHeight; curY++)
+	{
+		for(curX = 0; curX < theWidth; curX++)
+		{
+			// Super special case, 0,1 already solved
+			if ( (curX == 0) && (curY == 1))
+				continue;
+
+			bool foundTile = false;
+			for(auto possibleTile: remainingTiles)
+			{
+
+				for(int rot = 0; rot < 8; rot++)
+				{
+					possibleTile->rotateTile();
+
+					bool matchSuccess;
+					if (curX == 0)
+					{
+						// Special case, we are matching against the tile above us!
+						Tile* above = theMap[{0,curY - 1}];
+						matchSuccess = above->matchBottomSide(*possibleTile);
+					}
+					else
+					{
+						Tile* before = theMap[{curX - 1, curY}];
+						matchSuccess = before->matchRightSide(*possibleTile);
+					}
+
+					if (matchSuccess)
+					{
+						// We found the next tile
+						DEBUG << "Tile at " << curX << "," << curY << " is " << possibleTile->getTileNumber() << std::endl;
+						theMap[{curX,curY}] = possibleTile;
+
+						// Don't rotate this tile anymore!
+						foundTile = true;
+						rot = 8;
+						remainingTiles.erase(possibleTile);
+					}
+				}
+
+				if (foundTile)
+					break;
+			}
+		}
+	}
+
+
+}
+
+void TileMap::placeCornerTopLeft()
+{
+	DEBUG << "placeCornerTopLeft entered" << std::endl;
+	assert(theCornerList.size() == 4);
+
+	// Any corner can be top left due to all the rotations
+	std::pair<int, int> tlc = {0,0};
+
+	Tile* tlt = getTileByNumber(theCornerList[0]);
+	theMap[tlc] = tlt;
+	DEBUG << "Top Left Tile = " << tlt->getTileNumber() << std::endl;
+
+	std::vector<Tile*> nearbyTiles;
+	for(auto matchingTile: tlt->theMatchingTiles)
+	{
+		Tile* t = getTileByNumber(matchingTile);
+		nearbyTiles.push_back(t);
+		DEBUG << "Nearby tile: " << t->getTileNumber() << std::endl;
+	}
+
+	assert(tlt->theMatchingTiles.size() == 2);
+
+	// It only has 2 matches, 1 should match for the bottom, one for the right
+	for(int selfRotations = 0; selfRotations < 8; selfRotations++)
+	{
+		tlt->rotateTile();
+
+		for(int rightTileIndex = 0; rightTileIndex < 2; rightTileIndex++)
+		{
+			Tile* rt = nearbyTiles[rightTileIndex];
+			Tile* bt = nearbyTiles[(rightTileIndex + 1) % 2];
+
+			DEBUG << "Testing Right Tile = " << rt->getTileNumber() << ", and bottom tile = " 
+			      << bt->getTileNumber() << std::endl;
+
+			for(int rightTileRots = 0; rightTileRots < 8; rightTileRots++)
+			{
+				rt->rotateTile();
+
+				for(int bottomTileRots = 0; bottomTileRots < 8; bottomTileRots++)
+				{
+					bt->rotateTile();
+
+					if (tlt->matchRightSide(*rt) && tlt->matchBottomSide(*bt))
+					{
+						// Everything is rotated just right!
+
+						DEBUG << "Top Left Tile:" << std::endl;
+						tlt->printTile();
+						DEBUG << std::endl << "Bottom Tile: " << std::endl;
+						bt->printTile();
+						DEBUG << std::endl << "Right Tile:" << std::endl;
+						rt->printTile();
+
+						std::pair<int, int> rtc = {1,0};
+						std::pair<int, int> btc = {0,1};
+						theMap[rtc] = rt;
+						theMap[btc] = bt;
+						return;
+					}
+				}
+			}
+		}
+
+
+	}
+
+	DEBUG << "We tried all the rotations, can't make top-left work.  We are dumb" << std::endl;
+}
+
+TileMap::TileMap(std::vector<Tile*> tileSet)
+{
+	theTiles = tileSet;
+}
+
+void TileMap::findCorners()
+{
+	std::vector<int> numberOfOtherMatches;
+	for(int i = 0; i < theTiles.size(); i++)
+	{
+
+		int curMatches = 0;
+		for(int j = 0; j < theTiles.size(); j++)
+		{
+			DEBUG << "Comparing " << i << "(" << theTiles[i]->getTileNumber() << ") to "
+			      << j << "(" << theTiles[j]->getTileNumber() << std::endl;
+			
+			if (i == j)
+			{
+				continue;
+			}
+			
+			//DEBUG << "Intial state of J tile: " << std::endl;
+			//tileSet[j]->printTile();
+
+			for(int nrself = 0; nrself < 8; nrself++)
+			{
+				theTiles[i]->rotateTile();
+
+				for(int nrother = 0; nrother < 8; nrother++)
+				{
+					
+					if (theTiles[i]->matchRightSide(*theTiles[j]))
+					{
+						DEBUG << "MATCH:  tileSet" << i << "(" << theTiles[i]->getTileNumber() 
+						      << ") rotated " << nrself
+						      << " times, matches against tileSet" << j
+						      << "(" << theTiles[j]->getTileNumber() << ") rotated "
+						      << nrother << " times" << std::endl;
+						curMatches++;
+					}
+
+					theTiles[j]->rotateTile();
+					//DEBUG << "Other tile now rotated to :" << std::endl;
+					//tileSet[j]->printTile();
+				}
+			}
+		}
+
+		numberOfOtherMatches.push_back(curMatches);
+	}
+
+	// The corners will have 4 matches.  One match for each side, another match for the mirror of those same sides
+	for(int i = 0; i < theTiles.size(); i++)
+	{
+		if (numberOfOtherMatches[i] == 4)
+		{
+			// This is a corner
+			theCornerList.push_back(theTiles[i]->getTileNumber());
+		}
+	}
+
+}
+
+std::vector<int> TileMap::getCornerList()
+{
+	return theCornerList;
+}
+
+int Tile::getNumActiveCells()
+{
+	int retVal = 0;
+	for(int y = 0; y < theTileSize; y++)
+	{
+		for(int x = 0; x < theTileSize; x++)
+		{
+			if (getState(x,y))
+			{
+				retVal++;
+			}
+		}
+	}
+
+	return retVal;
+}
+
+int Tile::searchForSeaMonster()
+{
+	//                  #
+	//#    ##    ##    ###
+	// #  #  #  #  #  #
+	int retVal = 0;
+
+	static std::vector<std::pair<int, int> > seaMonsterOffsets;
+	const int SEA_MONSTER_WIDTH = 20;
+	const int SEA_MONSTER_HEIGHT = 3;
+
+	static bool oneTimeInit = true;
+
+	if(oneTimeInit)
+	{
+		std::vector<std::string> smdata;
+		smdata.push_back("                  # ");
+		smdata.push_back("#    ##    ##    ###");
+		smdata.push_back(" #  #  #  #  #  #   ");
+
+		for(int y = 0; y < smdata.size(); y++)
+		{
+			std::string curRow = smdata[y];
+			for(int x = 0; x < curRow.size(); x++)
+			{
+				if (curRow[x] == '#')
+				{
+					seaMonsterOffsets.push_back({x,y});
+				}
+			}
+		}
+
+	}
+
+	std::set<std::pair<int, int> > totalSeaMonsterHits;
+
+	for(int y = 0; y < theTileSize - SEA_MONSTER_HEIGHT; y++)
+	{
+		for(int x = 0; x < theTileSize - SEA_MONSTER_WIDTH; x++)
+		{
+			// X,Y is where we are testing for a sea monster
+
+			int smHits = 0;
+			for(auto smOffset: seaMonsterOffsets)
+			{
+				int smx = x + smOffset.first;
+				int smy = y + smOffset.second;
+				if (getState(smx,smy))
+				{
+					smHits++;
+				}
+			}
+
+			if (smHits == seaMonsterOffsets.size())
+			{
+				retVal++;
+
+				for(auto smOffset: seaMonsterOffsets)
+				{
+					int smx = x + smOffset.first;
+					int smy = y + smOffset.second;
+					if (getState(smx,smy))
+					{
+						totalSeaMonsterHits.emplace(smx, smy);
+					}
+				}
+			}
+		}
+	}
+
+	if (totalSeaMonsterHits.size())
+	{
+		// Show pretty picture of the sea monsters!
+		DEBUG << "WE HAVE SEA MONSTERS!!!!" << std::endl << std::endl;
+		for(int y = 0; y < theTileSize; y++)
+		{
+			for(int x = 0; x < theTileSize; x++)
+			{
+				if (totalSeaMonsterHits.find({x,y}) != totalSeaMonsterHits.end())
+				{
+					DEBUG << "O";
+				}
+				else
+				{
+					if (getState(x,y))
+					{
+						DEBUG << "#";
+					}
+					else
+					{
+						DEBUG << ".";
+					}
+				}
+			}
+
+			DEBUG << std::endl;
+		}
+	}
+
+	return totalSeaMonsterHits.size();
+
+}
+
+
+bool Tile::getState(int x, int y) const
+{ 
 	return theData[y][x] == '#';
 }
 
@@ -57,9 +545,32 @@ int Tile::getRotation() const
 
 bool Tile::matchRightSide(Tile const & rhs)
 {
-	for(int i = 0; i < TILE_SIZE; i++)
+	for(int i = 0; i < theTileSize; i++)
 	{
-		if (getState(TILE_SIZE-1, i) != rhs.getState(0,i) )
+		if (getState(theTileSize-1, i) != rhs.getState(0,i) )
+		{
+			return false;
+		}
+	}
+
+	// Every point on our right side matched left side of other tile
+	theMatchLog += "Match.  My rot=";
+	theMatchLog += std::to_string(theNumRotations);
+	theMatchLog += ",otherTile=";
+	theMatchLog += std::to_string(rhs.getTileNumber());
+	theMatchLog += ",otherTileRot=";
+	theMatchLog += std::to_string(rhs.getRotation());
+	theMatchLog += ". ";
+
+	theMatchingTiles.emplace(rhs.getTileNumber());
+	return true;
+}
+
+bool Tile::matchBottomSide(Tile const & rhs)
+{
+	for(int i = 0; i < theTileSize; i++)
+	{
+		if (getState(i, theTileSize-1) != rhs.getState(i,0) )
 		{
 			return false;
 		}
@@ -75,12 +586,18 @@ bool Tile::matchRightSide(Tile const & rhs)
 	theMatchLog += ". ";
 
 	return true;
+
 }
+
+
 
 Tile::Tile(std::vector<std::string> const & imageData)
 {
-	assert(imageData.size() == TILE_SIZE + 1);
-
+	theTileSize = imageData[1].size();
+	DEBUG << "TILE_SIZE = " << theTileSize << std::endl;
+	
+	assert(imageData.size() == theTileSize + 1);
+	
 	// Tiles are 10x10, with 1 row tile id header
 	std::vector<char> delimiters = { ' ', ':' };
 	std::vector<std::string> idParts = stringSplit(imageData[0], delimiters);
@@ -92,7 +609,7 @@ Tile::Tile(std::vector<std::string> const & imageData)
 
 	theTileNum = atoi(idParts[1].c_str());
 
-	for(int i = 1; i <= TILE_SIZE; i++)
+	for(int i = 1; i <= theTileSize; i++)
 	{
 		theData.push_back(imageData[i]);
 	}
@@ -119,20 +636,20 @@ void Tile::rotateTile()
 	std::vector<std::string> theOldData = theData;
 	theData.clear();
 
-	for(int y = 0; y < TILE_SIZE; y++)
+	for(int y = 0; y < theTileSize; y++)
 	{
 		std::string singleRow;
-		for(int x = 0; x < TILE_SIZE; x++)
+		for(int x = 0; x < theTileSize; x++)
 		{
 			if ( (theNumRotations == 3) || (theNumRotations == 7) )
 			{
 				// Special rotation case, the mirror flip
-				singleRow += theOldData[y][TILE_SIZE-x-1];
+				singleRow += theOldData[y][theTileSize-x-1];
 			}
 			else
 			{
 				// Rotations 90 deg clockwise
-				singleRow += theOldData[TILE_SIZE-x-1][y];
+				singleRow += theOldData[theTileSize-x-1][y];
 			}
 		}
 		theData.push_back(singleRow);
@@ -191,71 +708,57 @@ check_for_eof:
 
 	infile.close();
 
-	std::vector<int> numberOfOtherMatches;
-	for(int i = 0; i < tileSet.size(); i++)
+	TileMap tm(tileSet);
+	tm.findCorners();
+
+	std::vector<int> cornerList = tm.getCornerList();
+	uint64_t cornerProduct = 1;
+	for(auto ctn: cornerList)
 	{
-		/*std::vector<Tile> allRotations;
-		for(int nr = 0; nr < 8; nr++)
-		{
-			allRotations.push_back(*tileSet[i]);
-			tileSet[i]->rotateTile();
-		}*/
+		DEBUG << "Corner = " << ctn << std::endl;
+		cornerProduct *= (uint64_t) ctn;
+	}
+	
 
-		int curMatches = 0;
-		for(int j = 0; j < tileSet.size(); j++)
-		{
-			DEBUG << "Comparing " << i << "(" << tileSet[i]->getTileNumber() << ") to "
-			      << j << "(" << tileSet[j]->getTileNumber() << std::endl;
-			
-			if (i == j)
-			{
-				continue;
-			}
-			
-			//DEBUG << "Intial state of J tile: " << std::endl;
-			//tileSet[j]->printTile();
+	std::cout << "Part 1 = " << cornerProduct << std::endl;	
 
-			for(int nrself = 0; nrself < 8; nrself++)
-			{
-				tileSet[i]->rotateTile();
+	tm.placeCornerTopLeft();
+	tm.placeRemainingTiles();
 
-				for(int nrother = 0; nrother < 8; nrother++)
-				{
-					
-					if (tileSet[i]->matchRightSide(*tileSet[j]))
-					{
-						DEBUG << "MATCH:  tileSet" << i << "(" << tileSet[i]->getTileNumber() 
-						      << ") rotated " << nrself
-						      << " times, matches against tileSet" << j
-						      << "(" << tileSet[j]->getTileNumber() << ") rotated "
-						      << nrother << " times" << std::endl;
-						curMatches++;
-					}
+	tm.printEntireMap();
 
-					tileSet[j]->rotateTile();
-					//DEBUG << "Other tile now rotated to :" << std::endl;
-					//tileSet[j]->printTile();
-				}
-			}
-		}
+	std::vector<std::string> mtdata = tm.createMegaTile();
 
-		numberOfOtherMatches.push_back(curMatches);
+	DEBUG << "MEGA PREVIEW:" << std::endl;
+	for(auto mtrow: mtdata)
+	{
+		DEBUG << mtrow << std::endl;
 	}
 
-	uint64_t part1Sol = 1;
-	for(int i = 0; i < tileSet.size(); i++)
+
+	std::string megaLabel = "MEGATILE: 1:";
+	mtdata.insert(mtdata.begin(), megaLabel);
+	Tile mt(mtdata);
+	mt.printTile();
+
+	int numActiveCells = mt.getNumActiveCells();
+
+	for(int rot = 0; rot < 8; rot++)
 	{
-		DEBUG << "Tile: " << tileSet[i]->getTileNumber() << " = " << numberOfOtherMatches[i] << " matches" << std::endl;
-		DEBUG << "      " << tileSet[i]->theMatchLog << std::endl;
+		mt.rotateTile();
+		int sm = mt.searchForSeaMonster();
 
-		if (numberOfOtherMatches[i] == 4)
+
+		int stillActive = numActiveCells - sm;
+
+		DEBUG << "sm on rot" << rot << " = " << sm << ", still active = " << stillActive << std::endl;
+
+		if (sm)
 		{
-			DEBUG << tileSet[i]->getTileNumber() << " is a corner!" << std::endl;
-			part1Sol *= tileSet[i]->getTileNumber();
+			std::cout << "Part 2: " << stillActive << std::endl;
 		}
-	}
 
-	std::cout << "Part 1 = " << part1Sol << std::endl;	
+	}
 
 	return 0;
 }
