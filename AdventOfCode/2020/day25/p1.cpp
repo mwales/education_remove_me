bf2854f39603fd4187e7eeee8ae180e032e02268
Michael Wales <mwales3@gmail.com>
2021-01-01 03:38:47 -0500

    Cleaned up day 25 part1

--- AdventOfCode/2020/day25/p1.cpp
+++ AdventOfCode/2020/day25/p1.cpp
@@ -30,8 +30,6 @@ uint64_t findLoopCount(uint64_t subjectNum, uint64_t modulus, uint64_t keyVal)
 		{
 			break;
 		}
-
-		// DEBUG << "Loop " << loops << " = " << keyValCompute << std::endl;
 	}
 
 	DEBUG << "loop count = " << loops << std::endl;
@@ -46,8 +44,6 @@ uint64_t computeKeyVal(uint64_t subjectNum, uint64_t modulus, uint64_t loops, ui
 	{
 		keyValCompute *= subjectNum;
 		keyValCompute %= modulus;
-
-		// DEBUG << "Loop " << i << " = " << keyValCompute << std::endl;
 	}
 	
 	DEBUG << "Key computed after " << loops << " loops is " << keyValCompute << std::endl;
@@ -75,29 +71,13 @@ int main(int argc, char** argv)
 
 	uint64_t publicKey1 = strtoull(fileData[0].c_str(), NULL, 10);
 	uint64_t publicKey2 = strtoull(fileData[1].c_str(), NULL, 10);
-
-	
-	
-	// Testing stuff
 	 
 	uint64_t loopCount1 = findLoopCount(subjectNumber, modulus, publicKey1);
-	// uint64_t loopCount2 = findLoopCount(subjectNumber, modulus, publicKey2);
 
 	DEBUG << "loop count = " << loopCount1 << std::endl;
-	//nDEBUG << "loop count = " << loopCount2 << std::endl;
+	
+	uint64_t encKey = computeKeyVal(publicKey2, modulus, loopCount1, 1);
 
-	// uint64_t pubKey1 = computeKeyVal(subjectNumber, modulus, loopCount1, 1);
-	// uint64_t pubKey2 = computeKeyVal(subjectNumber, modulus, loopCount2, 1);
-
-	// DEBUG << "Computed pub key 1 = " << pubKey1 << std::endl;
-	// DEBUG << "Computed pub key 2 = " << pubKey2 << std::endl;
-
-	//uint64_t encKey = computeKeyVal(subjectNumber, modulus, loopCount1 * loopCount2, 1);
-	//uint64_t encKey2 = computeKeyVal(pubKey1, modulus, loopCount2, 1);
-	uint64_t encKey3 = computeKeyVal(publicKey2, modulus, loopCount1, 1);
-
-	//DEBUG << "Enc Key = " << encKey << std::endl;
-	//DEBUG << "Enc Key = " << encKey2 << std::endl;
-	DEBUG << "Enc Key = " << encKey3 << std::endl;
+	std::cout << "Enc Key = " << encKey << std::endl;
 	return 0;
 }
